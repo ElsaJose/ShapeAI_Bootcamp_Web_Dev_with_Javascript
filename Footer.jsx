d8cba28ff4439de6b836a557b4f995e41f2cb26d
ElsaJose <86551340+ElsaJose@users.noreply.github.com>
2021-06-27 14:36:13 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright by ShapeAI @ {new Date().getfullyear}</p>
+    </footer>
+  );
+}
+
+export default Footer;
